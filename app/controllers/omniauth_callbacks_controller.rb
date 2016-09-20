149dbd7bb1f3f53a1d3ac40cc90c2375fdf1f507
laomatt <laomatt1@gmail.com>
2016-09-19 20:17:59 -0700

    omniauth change

--- app/controllers/omniauth_callbacks_controller.rb
+++
@@ -1,3 +0,0 @@
-class Devise::OmniauthCallbacksController < ApplicationController
-
-end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Users::OmniauthCallbacksController < ApplicationController
 def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?

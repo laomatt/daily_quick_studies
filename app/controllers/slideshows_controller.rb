96c57649d809f2f23f9dbd27860719fb215900cd
laomatt <laomatt1@gmail.com>
2016-09-20 21:06:34 -0700

    fix auth

--- app/controllers/slideshows_controller.rb
+++ app/controllers/slideshows_controller.rb
@@ -1,6 +1,6 @@
 class SlideshowsController < ApplicationController
   before_action :set_slideshow, only: [:show, :edit, :update, :destroy, :draw_modal]
-  before_action :authenticate_user!, except: [:regen_rand, :draw_random]
+  before_action :authenticate_user!, except: [:regen_rand, :draw_random, :draw_modal, :draw_pose]
 
   # GET /slideshows
   # GET /slideshows.json

033143b000fc313fb1a65bf8d066b6d1ced707be
Matt L <matt@Matts-MacBook-Pro-2.local>
2017-02-19 15:25:19 -0800

    add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginate(:page => 1, :per_page => 8)
   end
 
   def welcome

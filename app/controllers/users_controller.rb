75c9e058f0799a83c5f59abb6b47dffb2cc94049
Matt L <matt@Matts-MacBook-Pro-2.local>
2017-02-19 15:31:03 -0800

    get rid of notice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.paginate(:page => 1, :per_page => 8)
+    @users = User.paginate(:page => params[:page], :per_page => 8)
   end
 
   def welcome

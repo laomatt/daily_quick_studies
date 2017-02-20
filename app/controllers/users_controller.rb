1a0e99dc51388be4e167d2548c2394126f97073a
Matt L <matt@Matts-MacBook-Pro-2.local>
2017-02-20 12:33:01 -0800

    fix search user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   end
 
   def search
-    users = User.where("name like ?","%#{params[:phrase]}%")
+    users = User.where("LOWER(name) LIKE '?'","%#{params[:phrase].downcase}%")
     render :partial => '/users/users_search_block', :locals => {:users => users}
   end
 
